b4f820541e1b727b5b562ced75479148a3a2d1a8
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-05-17 20:27:10 +0200

    add Horse entity

---
+++ app/Domain/Entities/Horse.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Domain\Models;
+
+/**
+ * Class Horse
+ * @package App\Domain\Models
+ */
+class Horse
+{
+    /**
+     * @var float
+     */
+    private $speed;
+
+    /**
+     * @var float
+     */
+    private $speedShortage;
+
+    /**
+     * @var float
+     */
+    private $strength;
+
+    /**
+     * @var float
+     */
+    private $endurance;
+
+    /**
+     * @param float $speed
+     */
+    public function setSpeed(float $speed)
+    {
+        $this->speed = $speed;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSpeedShortage(): float
+    {
+        return $this->speedShortage;
+    }
+
+    /**
+     * @param float $speedShortage
+     */
+    public function setSpeedShortage(float $speedShortage)
+    {
+        $this->speedShortage = $speedShortage;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSpeed(): float
+    {
+        return $this->speed;
+    }
+
+    /**
+     * @param float $strength
+     */
+    public function setStrength(float $strength)
+    {
+        $this->strength = $strength;
+    }
+
+    /**
+     * @return float
+     */
+    public function getStrength(): float
+    {
+        return $this->strength;
+    }
+
+    /**
+     * @param float $endurance
+     */
+    public function setEndurance(float $endurance)
+    {
+        $this->endurance = $endurance;
+    }
+
+    /**
+     * @return float
+     */
+    public function getEndurance(): float
+    {
+        return $this->endurance;
+    }
+
+}
