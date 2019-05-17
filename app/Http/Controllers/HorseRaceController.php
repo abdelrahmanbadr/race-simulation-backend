39566a461870687f02f3b125b570f273a6b9d841
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-05-17 22:34:55 +0200

    make domain controller

---
+++ app/Http/Controllers/HorseRaceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HorseRaceController extends Controller
+{
+    public function createRace()
+    {
+
+    }
+
+    public function getActiveRaces()
+    {
+
+    }
+}
